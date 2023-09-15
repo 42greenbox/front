c8f737c79d598ae6fc3c5f7670e0da64d1538ec3
sohan <sohan@student.42seoul.kr>
2023-09-15 13:40:50 +0900

    Feat: fetch repository 추가후 DI시 fetch repository 쓰도록 변경

---
+++ infrastructure/FetchItemRepository.ts
@@ -0,0 +1,75 @@
+import { IItemRepository } from "@/domain/IItemRepository";
+import { ItemDto, ItemSendType, ItemType } from "@/domain/Item";
+import { UserType } from "@/domain/User";
+
+export default class FetchItemRepository implements IItemRepository {
+  public getItems = async () => {
+    const res = await fetch("https://server.42greenbox.com/storage");
+    const rawItems: ItemDto[] = await res.json();
+    const items: ItemType[] = rawItems.map((dto) => {
+      return {
+        id: dto.item_id,
+        img: dto.img,
+        owner: dto.user_id,
+        title: dto.title,
+        expiredAt: dto.expiredAt,
+        location: dto.location,
+        share: dto.share,
+        rental: dto.rental,
+      };
+    });
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("Failed to fetch data");
+    }
+
+    return items;
+  };
+  public getMyItems = async () => {
+    const res = await fetch("https://server.42greenbox.com/storage/me");
+    const rawItems: ItemDto[] = await res.json();
+    const items: ItemType[] = rawItems.map((dto) => {
+      return {
+        id: dto.item_id,
+        img: dto.img,
+        owner: dto.user_id,
+        title: dto.title,
+        expiredAt: dto.expiredAt,
+        location: dto.location,
+        share: dto.share,
+        rental: dto.rental,
+      };
+    });
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("Failed to fetch data");
+    }
+
+    return items;
+  };
+
+  public getItem = async (item: ItemType) => {
+    const res = await fetch(
+      `/storage?user_id=${item.owner}&item_id=${item.id}`
+    );
+    const rawItem: ItemDto = await res.json();
+    const itemm: ItemType = {
+      id: rawItem.item_id,
+      img: rawItem.img,
+      owner: rawItem.user_id,
+      title: rawItem.title,
+      expiredAt: rawItem.expiredAt,
+      location: rawItem.location,
+      share: rawItem.share,
+      rental: rawItem.rental,
+    };
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("Failed to fetch data");
+    }
+    return itemm;
+  };
+
+  public putItem(item: ItemType, status: string): void {}
+  public postItem(user: UserType, item: ItemSendType): void {}
+}
--- lib/DI.ts
+++ lib/DI.ts
@@ -1,11 +1,11 @@
+import FetchItemRepository from "@/infrastructure/FetchItemRepository";
 import { IItemRepository } from "../domain/IItemRepository";
 import { IUserRepository } from "../domain/IUserRepository";
-import ItemRepository from "../infrastructure/ItemRepository";
 import UserRepository from "../infrastructure/UserRepository";
 
 //TODO: Repository 실제로 DI 할 수 있게 (인자로 어떤 레포지토리를 쓸지 받아올 수 있게) 변경
 class DI {
-  private static ItemRepository: IItemRepository = new ItemRepository();
+  private static ItemRepository: IItemRepository = new FetchItemRepository();
   private static UserRepository: IUserRepository = new UserRepository();
 
   public static getItemRepository = () => {
