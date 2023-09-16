ab02ae1c366722d792eff7b686bb85a04ca80673
sohan <sohan@student.42seoul.kr>
2023-09-16 10:19:28 +0900

    Feat: route handler test

--- lib/FetchInstance.ts
+++ lib/FetchInstance.ts
@@ -1,38 +1,9 @@
-import returnFetch, { FetchArgs } from "return-fetch";
-import { deleteCookie, getCookie } from "./cookie";
+import returnFetch from "return-fetch";
 
 const headers: HeadersInit | undefined = {
   Accept: "*/*",
   "Content-Type": "application/json; charset=utf-8",
   "X-Requested-With": "XMLHttpRequest",
-};
-
-const injectToken = (args: FetchArgs) => {
-  try {
-    const requestInit: RequestInit = args[1]!;
-    const headers = requestInit.headers!;
-    const token = localStorage.getItem("accessToken");
-
-    console.log("token", token);
-    //const token = getCookie("csrftoken");
-    if (token != null) {
-      console.log("쿠키설정 완");
-    } else {
-      const token = getCookie("token");
-      console.log("notoken", token);
-      if (token != null) {
-        deleteCookie("token");
-        localStorage.setItem("accessToken", token);
-      }
-    }
-    const newHeaders = {
-      ...headers,
-      Autorization: `Bearer ${token}`,
-    };
-    return newHeaders;
-  } catch (error) {
-    throw new Error(error as string);
-  }
 };
 
 const fetchInstance = returnFetch({
@@ -43,13 +14,7 @@ const fetchInstance = returnFetch({
       console.log("********before sending request*********");
       console.log("url:", args[0].toString());
       console.log("requestInit:", args[1], "\n\n");
-      const newHeaders: HeadersInit = injectToken(args);
-      const newArgs = {
-        ...args,
-        headers: newHeaders,
-      };
-      console.log("**********after inject token**********", newArgs);
-      return newArgs;
+      return args;
     },
     response: async (response, requestArgs) => {
       console.log("********after receiving response*********");
