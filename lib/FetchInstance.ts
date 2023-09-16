79117846d364073a6e38495dfdc843274e3cf76d
sohan <sohan@student.42seoul.kr>
2023-09-16 09:07:35 +0900

    Notworking: 서버단에서 돌아가고있기때문에 window 전역 객체에 접근 못함. token injection은 nextjs route handler를 이용하자

--- lib/FetchInstance.ts
+++ lib/FetchInstance.ts
@@ -1,9 +1,38 @@
-import returnFetch from "return-fetch";
+import returnFetch, { FetchArgs } from "return-fetch";
+import { deleteCookie, getCookie } from "./cookie";
 
 const headers: HeadersInit | undefined = {
   Accept: "*/*",
   "Content-Type": "application/json; charset=utf-8",
   "X-Requested-With": "XMLHttpRequest",
+};
+
+const injectToken = (args: FetchArgs) => {
+  try {
+    const requestInit: RequestInit = args[1]!;
+    const headers = requestInit.headers!;
+    const token = localStorage.getItem("accessToken");
+
+    console.log("token", token);
+    //const token = getCookie("csrftoken");
+    if (token != null) {
+      console.log("쿠키설정 완");
+    } else {
+      const token = getCookie("token");
+      console.log("notoken", token);
+      if (token != null) {
+        deleteCookie("token");
+        localStorage.setItem("accessToken", token);
+      }
+    }
+    const newHeaders = {
+      ...headers,
+      Autorization: `Bearer ${token}`,
+    };
+    return newHeaders;
+  } catch (error) {
+    throw new Error(error as string);
+  }
 };
 
 const fetchInstance = returnFetch({
@@ -14,7 +43,13 @@ const fetchInstance = returnFetch({
       console.log("********before sending request*********");
       console.log("url:", args[0].toString());
       console.log("requestInit:", args[1], "\n\n");
-      return args;
+      const newHeaders: HeadersInit = injectToken(args);
+      const newArgs = {
+        ...args,
+        headers: newHeaders,
+      };
+      console.log("**********after inject token**********", newArgs);
+      return newArgs;
     },
     response: async (response, requestArgs) => {
       console.log("********after receiving response*********");
